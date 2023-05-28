1b75c422a491e9162f992501117cbaa49b9fb57f
ThejasNU <thejasnu@gmail.com>
2023-05-28 21:29:24 +0530

    added routes for department

---
+++ department-service/src/main/java/com/tnu/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,38 @@
+package com.tnu.departmentservice.controller;
+
+import com.tnu.departmentservice.model.Department;
+import com.tnu.departmentservice.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+    @Autowired
+    private DepartmentRepository repository;
+
+    @GetMapping
+    public List<Department> getAll(){
+        LOGGER.info("Department find");
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Department add(@RequestBody Department department){
+        LOGGER.info("Department add: {}",department);
+        return repository.addDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department getById(@PathVariable Long id){
+        LOGGER.info("Department find: id={}",id);
+        return repository.findById(id);
+    }
+}
